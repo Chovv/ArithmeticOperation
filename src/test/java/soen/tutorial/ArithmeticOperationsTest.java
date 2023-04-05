070c297775bf60ea3337b16baf3702bd310bcb0b
William Chong <80657298+Chovv@users.noreply.github.com>
2023-04-04 21:38:06 -0400

    MustFail

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.fail;
+
 public class ArithmeticOperationsTest {
 
 
@@ -15,6 +17,7 @@ public class ArithmeticOperationsTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 		// assertEquals(expected, actual);
+		fail("must fail");
 
 	}
     //@Test
